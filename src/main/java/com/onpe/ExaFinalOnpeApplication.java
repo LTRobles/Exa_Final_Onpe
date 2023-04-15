b53305254e3473eb7e03909ccc54ea4d67f6fbd6
Your Name <you@example.com>
2023-04-15 11:25:43 -0500

    actualizaondo

---
+++ src/main/java/com/onpe/ExaFinalOnpeApplication.java
@@ -0,0 +1,13 @@
+package com.onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaFinalOnpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaFinalOnpeApplication.class, args);
+	}
+
+}
