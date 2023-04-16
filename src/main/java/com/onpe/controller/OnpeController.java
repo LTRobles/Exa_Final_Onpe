8316d58100544aeb3b9feb0e5011c9afc97e3c1a
Your Name <you@example.com>
2023-04-16 00:39:53 -0500

    actualizando5.0

--- src/main/java/com/onpe/controller/OnpeController.java
+++ src/main/java/com/onpe/controller/OnpeController.java
@@ -1,11 +1,17 @@
 package com.onpe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.onpe.services.IGrupoVotacionService;
+
 
 @Configuration
 @CrossOrigin
@@ -22,7 +28,29 @@ public String index() {
 	return "index";
 }
 
+@GetMapping ("/actas")
+
+public String actas() {
+	
+	return "actas";
 }
 
+@GetMapping ("/actas_info")
+
+public String actas_info() {
+	
+	return "actas_info";
+}
+@Autowired
+private IGrupoVotacionService iGrupoVotacionService;
+
+@GetMapping ("/actas_info/{id}")
+public String listar (Model modelo, @PathVariable ("id")String id) {
+modelo.addAttribute("actas", iGrupoVotacionService.getGrupoVotacion(id));
+return "actas_info";
+
+}
+
+}
 
 
